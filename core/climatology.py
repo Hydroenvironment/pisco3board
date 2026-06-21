0a7d899830ba5e887073777bafc11d371673fdbd
Julio Isaac Montenegro Gambini <jmontenegrog@uni.edu.pe>
2026-06-21 22:55:46 +0200

    Delete core directory

--- core/climatology.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def climatology(ts, period=365):
-    ts = np.array(ts)
-    return pd.Series(ts).groupby(np.arange(len(ts)) % period).mean()
--- core/data_loader.py
+++
@@ -1,5 +0,0 @@
-import xarray as xr
-
-def load_dataset(url):
-    ds = xr.open_dataset(url, chunks={'Z1': 1000})
-    return ds
